6ed4b37889acc83170dfa2918c5dcb4af4ac02b2
Merve Çavuşoğlu <merwecvss@gmail.com>
2025-03-11 22:03:30 +0300

    Add files via upload

---
+++ Sayı/Sayi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program{
+    static void Main(){
+        Console.Write("Bir Sayı Giriniz: ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+        if (sayi<0)
+        Console.WriteLine("Girilen Sayı Negatiftir.");
+        else if (sayi>0)
+        Console.WriteLine("Girilen Sayı Pozitiftir.");
+        else 
+        Console.WriteLine("Girilen Sayı 0'a eşittir.");
+    }
+}
