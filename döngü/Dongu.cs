4609bc9433628c4ba320616e485123d920175013
Merve Çavuşoğlu <merwecvss@gmail.com>
2025-03-11 22:32:28 +0300

    Add files via upload

---
+++ döngü/Dongu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program{
+    static void Main(){
+        Console.Write("Bir Sayı Giriniz: ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+
+        int toplamfor = 0;
+        for (int i = 1; i <= sayi; i++){
+            toplamfor += i;
+        }
+        Console.WriteLine($"For Döngüsü İle Toplam: {toplamfor}");
+
+        int toplamwhile = 0;
+        int j = 1;
+        while (j <= sayi){
+            toplamwhile +=j;
+            j++;
+        } 
+        Console.WriteLine($"While Döngüsü İle Toplam: {toplamwhile}");
+    }
+}
+
