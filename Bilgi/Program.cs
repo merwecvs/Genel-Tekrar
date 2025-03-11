cef81db247749692078adb9c20715630c2f665de
Merve Çavuşoğlu <merwecvss@gmail.com>
2025-03-11 21:55:32 +0300

    Add files via upload

---
+++ Bilgi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program{
+    static void Main(){
+        Console.Write("Adınızı Giriniz: ");
+        string ad = Console.ReadLine();
+        Console.Write("Soyadınızı Giriniz: ");
+        string soyad = Console.ReadLine();
+        Console.Write("Yaşınızı Giriniz: ");
+        int yas = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Merhaba {ad} {soyad}, {yas} yaşındasınız. Hoşgeldiniz!");
+    }
+}
